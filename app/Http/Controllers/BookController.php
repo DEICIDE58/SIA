cc4071c0bb71e4eb9dc551c4cbb67211aeb50e13
Daniela Limbaga <limbagadaniela2@gmail.com>
2025-09-05 00:52:04 +0800

    get all book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Books;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    // Get all book
+    public function getAll()
+    {
+        return response()->json(books::all());
+    }
 }
