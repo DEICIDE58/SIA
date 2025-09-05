b264e3015845a2cae04bcc11afe20093f613e6c3
Daniela Limbaga <limbagadaniela2@gmail.com>
2025-09-06 00:09:01 +0800

    making default book

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,16 +2,20 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Books; 
 
 class BookSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $books = [
+            ['name' => 'Harry Potter', 'summary' => 'A boy with sunglasses.'],
+
+        ];
+
+        foreach ($books as $book) {
+            Books::create($book);
+        }
     }
 }
